775718ff3e60ddac2bb04c856e32968984c22c4d
eotwoma <edinah.otwoma@gmail.com>
2017-05-09 00:41:12 +0300

    added changes on negative numbers

--- generate_prime.py
+++ generate_prime.py
@@ -1,8 +1,10 @@
 def generateprime():
 	for x in xrange(1,n):
 		if x == 1:
-			return "One is not Prime"
+			return "One cannot be a prime number."
+		elif x < 1:
+			print("Not possible to generate prime numbers for ")
 		elif (x%1) == 0:
-			print(x, 'is a prime Number')
+			print(x, 'is not a prime Number')
 		else:
 			print(x, 'is a prime Number')
