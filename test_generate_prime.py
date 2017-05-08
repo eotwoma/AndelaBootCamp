6c15ab00f0f29b2cbd02acab0fb16ecd7c64b9d3
eotwoma <edinah.otwoma@gmail.com>
2017-05-09 00:43:05 +0300

    new changes on test cases

--- test_generate_prime.py
+++ test_generate_prime.py
@@ -1,4 +1,47 @@
 import unittest
+from prime_numbers import prime_numbers
 class TestPrime numbers(unittest.TestCase):
-	def test_prime(self):
-		self.assertEqual(2,num)
+
+	 def test_prime_numbers(self):
+        """Testing if prime numbers are correctly generated."""
+
+        self.assertEqual(prime_numbers(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+    def test_two_hundred(self):
+        """Testing if prime numbers are correctly generated."""
+
+        self.assertEqual(prime_numbers(200),
+                         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+                          97, 101, 103, 107, 109, 113,
+                          127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199])
+
+    def test_one(self):
+        """"Testing if one is correctly determined not to be prime."""
+
+        self.assertEqual(prime_numbers(
+            1), "One cannot be prime numbers.")
+
+    def test_two(self):
+        """Testing if error returned if integer entered is 2."""
+
+        self.assertEqual(prime_numbers("String"), "Only integers are allowed.")
+
+    def test_invalid_type_string_list(self):
+        """Testing if error returned if input not integer."""
+
+        self.assertEqual(prime_numbers([]), "Only integers are allowed.")
+
+    def test_invalid_type_string_set(self):
+        """Testing if error returned if input not integer."""
+
+        self.assertEqual(prime_numbers(set()), "Only integers are allowed.")
+
+    def test_only_positive(self):
+        """Testing if error returned if negative integers input."""
+
+        self.assertEqual(
+            prime_numbers(-1), "Not possible to generate prime numbers for integers less than 2.")
+
+
+if __name__ == "__main__":
+unittest.main()
