fed23929591a700c4d957a1fedd30b7152e77f9d
eotwoma <edinah.otwoma@gmail.com>
2017-05-08 22:16:48 +0300

    prime numbers

---
+++ generate_prime.py
@@ -0,0 +1,8 @@
+def generateprime():
+	for x in xrange(1,n):
+		if x == 1:
+			return "One is not Prime"
+		elif (x%1) == 0:
+			print(x, 'is a prime Number')
+		else:
+			print(x, 'is a prime Number')
